fc2eb6eb756a697ae92a22c18ab96a8e1b5db36d
Mar_Veloz <>
2025-09-20 16:11:34 -0400

    add better silk hearts mod

---
+++ better-silk-hearts/Plugin.cs
@@ -0,0 +1,27 @@
+using BepInEx;
+using HarmonyLib;
+
+namespace better_silk_hearts;
+
+[BepInAutoPlugin(id: "org.mar_veloz.silksong.better_silk_hearts")]
+public partial class Plugin : BaseUnityPlugin
+{
+    private const int SilkRegenMaxMultiplier = 3;
+
+    private void Awake()
+    {
+        Harmony.CreateAndPatchAll(typeof(Plugin));
+
+        Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
+    }
+
+    [HarmonyPatch(typeof(PlayerData), nameof(PlayerData.CurrentSilkRegenMax), MethodType.Getter)]
+    [HarmonyPrefix]
+    private static void CurrentSilkRegenMaxReplacement(ref PlayerData __instance, ref int __result, ref bool __runOriginal)
+    {
+        __result = __instance.silkRegenMax * SilkRegenMaxMultiplier;
+
+        // Skip running the original method
+        __runOriginal = false;
+    }
+}
